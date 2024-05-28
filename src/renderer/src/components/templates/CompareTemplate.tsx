1d7c9fe5d56bff418c5ea5b73df8fe777cf306b7
Jinan Jeong <jinanjeong@snu.ac.kr>
2024-05-28 23:24:49 +0900

    feat: implement CompareTemplate grid

--- src/renderer/src/components/templates/CompareTemplate.tsx
+++ src/renderer/src/components/templates/CompareTemplate.tsx
@@ -1,14 +1,42 @@
 import { FC } from 'react';
 import SourceSelector from '../organisms/SourceSelector';
 import TextInput from '../organisms/TextInput';
-import SelectCheckbox from '../atoms/select/SelectCheckbox';
+import MultiSelectChip from '../molecules/MultiSelectChip';
 
 const CompareTemplate: FC = () => {
   return (
-    <div className="flex flex-wrap justify-between gap-4">
+    <div className="flex flex-wrap items-start justify-start gap-16">
       <SourceSelector label="Comparison Subject" required />
-      <div className="flex flex-col gap-10">
-        <TextInput label="Storage path for comparison results" required options={[]} />
+      <div className="flex flex-col justify-start gap-10">
+        <TextInput
+          label="Storage path for comparison results"
+          required
+          options={[{ type: 'file', label: 'Local path', value: 'local', placeholder: '~/' }]}
+        />
+        <TextInput
+          label="File name and format of comparison results"
+          required
+          dropdown={false}
+          options={[
+            {
+              type: 'text',
+              label: 'File name',
+              value: 'filename',
+              placeholder: 'Enter the desired file name.'
+            }
+          ]}
+          suffix={
+            <MultiSelectChip
+              options={[
+                { value: 'excel', label: '.excel' },
+                { value: 'json', label: '.json' },
+                { value: 'yaml', label: '.yaml' },
+                { value: 'html', label: '.html' }
+              ]}
+              onChange={(selectedValues) => console.log(selectedValues)}
+            />
+          }
+        />
       </div>
     </div>
   );
