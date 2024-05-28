f42a94fd9e3a1571a6bc1d19f98a0abe84e7fa7c
조유진 <uzinfamily@snu.ac.kr>
2024-05-28 20:09:45 +0900

    fix: titlebar draggability problem, toggle color problem

--- src/renderer/src/components/molecules/ListBoxItem.tsx
+++ src/renderer/src/components/molecules/ListBoxItem.tsx
@@ -12,9 +12,9 @@ interface IListBoxItemProps {
 
 const ListBoxItem: FC<IListBoxItemProps> = ({ option, path, onEditClick, onRemoveClick }) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="w-[330px] h-9 p-1 bg-white rounded-md justify-start items-center gap-1.5 inline-flex">
       {option == 'local' ? <Localchip /> : <Githubchip />}
-      <div className="self-stretch text-[11px] font-normal">{path}</div>
+      <div className="w-[204px] grow shrink basis-0 flex flex-col justify-center items-start gap-1.5 self-stretch text-PaleGray-900 text-[11px] font-normal">{path}</div>
       <IconButton onClick={onEditClick}>e</IconButton>
       <IconButton onClick={onRemoveClick}>d</IconButton>
     </div>
@@ -22,3 +22,18 @@ const ListBoxItem: FC<IListBoxItemProps> = ({ option, path, onEditClick, onRemov
 }
 export default ListBoxItem;
 
+
+<div className="w-[330px] h-9 p-1 bg-white rounded-md justify-start items-center gap-1.5 inline-flex">
+<div className="h-[22px] p-1 bg-emerald-50 rounded-md justify-center items-center gap-2.5 flex">
+<div className="text-teal-600 text-[11px] font-normal font-['Spoqa Han Sans Neo']">Tag</div>
+</div>
+<div className="w-[204px] grow shrink basis-0 flex-col justify-center items-start gap-1.5 inline-flex">
+<div className="self-stretch text-zinc-700 text-[11px] font-normal font-['Spoqa Han Sans Neo']">Path</div>
+</div>
+<div className="w-7 h-7 bg-gray-100 rounded-md justify-center items-center flex">
+<div className="w-4 h-4 relative" />
+</div>
+<div className="w-7 h-7 bg-gray-100 rounded-md justify-center items-center flex">
+<div className="w-4 h-4 relative" />
+</div>
+</div>
--- src/renderer/src/components/organisms/AppBar.tsx
+++ src/renderer/src/components/organisms/AppBar.tsx
@@ -23,14 +23,14 @@ const AppBar: FC = () => {
   return (
     <>
       <div
-        className="main-container z-40"
+        className="main-container z-40 draggable"
         style={{
           position: 'fixed',
           width: '100%',
           top: 0
         }}
       >
-        <div className="title-bar">
+        <div className="title-bar draggable ">
           <div className="title prevent-select draggable">
             <img src="./src/assets/titlebar/title-logo.svg" alt="fosslogo" className="title-logo" />
             <div style={{ fontFamily: 'Segoe UI, sans-serif' }} className="title-text">
@@ -39,21 +39,22 @@ const AppBar: FC = () => {
             <img src="./src/assets/titlebar/title-bar-icon.png" />
             <div className="version-text">v1.7.22</div>
           </div>
-          <div className="control prevent-select">
-            <button id="setting" onClick={openModal}>
+
+          <div className="control prevent-select ">
+            <button id="setting" onClick={openModal} className="no-drag">
               <img src="./src/assets/titlebar/title-setting-icon.png" />
             </button>
-            <button id="info">
+            <button id="info" className="no-drag">
               <img src="./src/assets/titlebar/title-info-icon.png" />
             </button>
             <img src="./src/assets/titlebar/title-bar-icon.png" />
-            <button onClick={handleClickMinimizeButton}>
+            <button onClick={handleClickMinimizeButton} className="no-drag">
               <img src="./src/assets/titlebar/title-min-icon.png" alt="minimize" />
             </button>
-            <button onClick={handleClickMaximizeButton}>
+            <button onClick={handleClickMaximizeButton} className="no-drag">
               <img src="./src/assets/titlebar/title-max-icon.png" alt="max" />
             </button>
-            <button onClick={handleClickCloseButton}>
+            <button onClick={handleClickCloseButton} className="no-drag">
               <img src="./src/assets/titlebar/title-close-icon.png" alt="close" />
             </button>
           </div>
