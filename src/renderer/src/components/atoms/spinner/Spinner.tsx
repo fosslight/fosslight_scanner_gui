2f63a429ba227ad46e6fa2bde0b60a94487448e6
Jinan Jeong <jinanjeong@snu.ac.kr>
2024-06-01 00:36:19 +0900

    feat: start implementing Spinner

---
+++ src/renderer/src/components/atoms/spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Spinner: FC = () => {
+  return (
+    <div className="h-6 w-6 animate-spin rounded-full border-2 border-solid border-Blue-500"></div>
+  );
+};
+
+export default Spinner;
