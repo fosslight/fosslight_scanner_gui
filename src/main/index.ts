e93d532089a9e6f5ba7365876ba3c3cc879ca119
SeongjunJo <gukcvts679@snu.ac.kr>
2024-06-05 22:22:49 +0900

    feat: install venv and run fosslight_scanner
    Signed-off-by: SeongjunJo <gukcvts679@snu.ac.kr>

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,9 +3,12 @@ import { dirname, join, resolve } from 'path';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png';
 import { fileURLToPath } from 'url';
+import BinaryExecuter from './src/BinaryExecuter';
+import commandParser from './src/CommandParser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const binaryExecuter = BinaryExecuter.getInstance();
 
 let mainWindow: BrowserWindow;
 let hiddenWindow: BrowserWindow;
@@ -67,7 +70,7 @@ function createHiddenWindow(): void {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron');
 
@@ -81,15 +84,44 @@ app.whenReady().then(() => {
   createWindow();
   createHiddenWindow();
 
+  const arg = !binaryExecuter.checkVenv() ? 'false' : undefined; // assign any string is fine
+  console.log('Waiting for setting venv and Fosslight Scanner');
+  const progressInterval = setInterval(() => {
+    process.stdout.write('.');
+  }, 500); // 설치하는 동안 500ms 간격으로 점 출력
+
+  // Will take a long time (about 3 min) when the first install the venv and fs.
+  const setVenv: boolean = await binaryExecuter.executeSetVenv(arg);
+
+  if (!setVenv) {
+    console.error(
+      '[Error]: Failed to set venv and install Fosslight Scanner.\n\t Please check the resources folder and files are in initial condition.\n\t Or try to reinstall this app.'
+    );
+  } else {
+    console.log('Fosslight Scanner is ready to use.');
+  }
+  clearInterval(progressInterval); // 점 출력 정지
+
   // IPC communication between main and hidden windows
-  ipcMain.on('send-command', (event, { command }) => {
-    console.log('command: ', command);
-    const message =
-      command.type === 'analyze'
-        ? 'Analyze command executed successfully'
-        : 'Compare command executed successfully';
-    event.reply('recv-command-result', message);
-    hiddenWindow.webContents.send('recv-command', { command });
+  ipcMain.on('send-command', async (event, { command }) => {
+    const args: string[] = commandParser.parseCommand(command);
+
+    // check venv and fs before executing.
+    if (!binaryExecuter.checkVenv()) {
+      console.error(
+        '[Error]: Failed to run Fosslight Scanner.\n\t Please check the resources folder and files are in initial condition.\n\t Or try to reinstall this app.'
+      );
+    } else {
+      const result: string = await binaryExecuter.executeScanner(args);
+
+      const message =
+        command.type === 'analyze'
+          ? 'Analyze command executed successfully'
+          : 'Compare command executed successfully';
+
+      event.reply('recv-command-result', result); // 화면에 보임
+      hiddenWindow.webContents.send('recv-command', { result }); // 콘솔에 찍힘
+    }
   });
 
   ipcMain.on('send-log', (_, { log }) => {
