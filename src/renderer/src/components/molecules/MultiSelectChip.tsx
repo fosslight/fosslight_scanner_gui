1ba31236aa3e5b9927dbd21f833e35f993989131
Jinan Jeong <jinanjeong@snu.ac.kr>
2024-06-06 02:01:53 +0900

    feat: implement internal state management in MultiSelectChip

--- src/renderer/src/components/molecules/MultiSelectChip.tsx
+++ src/renderer/src/components/molecules/MultiSelectChip.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import SelectChip from '../atoms/select/SelectChip';
 
 export interface ISelectChipOption {
@@ -13,11 +13,18 @@ interface IMultiSelectChipProps {
 }
 
 const MultiSelectChip: FC<IMultiSelectChipProps> = ({ options, values, onChange }) => {
+  const [selectedValues, setSelectedValues] = useState<Set<string>>(values ?? new Set());
+
   const handleSelect = (value: string) => (selected: boolean) => {
-    const newSelectedValues = new Set(values ?? []);
+    const newSelectedValues = new Set(selectedValues);
     selected ? newSelectedValues.add(value) : newSelectedValues.delete(value);
+    setSelectedValues(newSelectedValues);
     onChange?.(newSelectedValues);
   };
+
+  useEffect(() => {
+    setSelectedValues(values ?? new Set());
+  }, [values]);
 
   return (
     <div className="flex items-start gap-2">
@@ -25,7 +32,7 @@ const MultiSelectChip: FC<IMultiSelectChipProps> = ({ options, values, onChange 
         <SelectChip
           key={option.value}
           title={option.label}
-          selected={values?.has(option.value)}
+          selected={selectedValues?.has(option.value)}
           onSelect={handleSelect(option.value)}
         />
       ))}
