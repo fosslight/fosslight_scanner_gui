3629568e79f954f0dd2525af76b73a6fdb2df5ae
Jinan Jeong <jinanjeong@snu.ac.kr>
2024-07-10 00:48:10 +0900

    feat: revise FossLogo size on AppInitializer

--- src/renderer/src/components/organisms/AppInitializer.tsx
+++ src/renderer/src/components/organisms/AppInitializer.tsx
@@ -5,8 +5,8 @@ import { FossLogo } from '../atoms/SVGIcons';
 const AppInitializer: FC = () => {
   return (
     <div className="draggable fixed h-full w-full">
-      <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-        <FossLogo />
+      <div className="flex h-full w-full flex-col items-center justify-center gap-5">
+        <FossLogo width={40} height={40} />
         <Text type="p400-m" color="white">
           Getting Ready...
         </Text>
